7a863b56ecd2e4c5a29fd4cc9ba12022c8c06280
konsta toivanen <konsta.13c@gmail.com>
2026-04-05 23:49:28 +0300

    remove redundant animations.

--- script_panels.js
+++ script_panels.js
@@ -289,7 +289,7 @@ function expand_tile(id, do_focus)
         case "local_image":
 			if (try_update_previous_tile(id))
 			{
-				element.insertAdjacentHTML("afterend", "<div id='expand_div' class='tile_parent' style='animation-name:ClipIn_Left_TileExtendedTall'><div class='image_wide'><img src='" + element.dataset.content + "'/></div></div>");
+				element.insertAdjacentHTML("afterend", "<div id='expand_div' class='tile_parent' style='animation-name:ClipIn_Left_Img'><div class='image_wide'><img src='" + element.dataset.content + "'/></div></div>");
 				break;
 			}
             return;
