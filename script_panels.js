c9a18fb0b3ca0f1e06975319a27dbd3418a4a936
konsta toivanen <konsta.13c@gmail.com>
2026-04-05 04:52:34 +0300

    expand tiles in reverse order.

--- script_panels.js
+++ script_panels.js
@@ -234,18 +234,20 @@ function update_subpage()
         console.log(initialId);
     }
 	
-    setTimeout(function () { expand_tile("tile_id_" + initialId); }, 250);
-	
 	if (panel != null && panel.hasAttribute("data-unfold"))
 	{	
-		for (var i = 1; i < tiles.length; i++)
+		for (var i = tiles.length - 1; i >= 0; i--)
 		{			
 			if (tiles.item(i).dataset.content != null)
 			{
 				const tileid = tiles.item(i).id;
-				tiles.item(i).dataset.tweenTimeoutHandle = setTimeout(function () { expand_tile(tileid.toString()); }, 350 * (i + 1));
+				tiles.item(i).dataset.tweenTimeoutHandle = setTimeout(function () { expand_tile(tileid.toString()); }, 250 * (tiles.length - i));
 			}
 		}
+	}
+	else
+	{
+		setTimeout(function () { expand_tile("tile_id_" + initialId); }, 250);
 	}
 }
 
